38d480fce5176d8c72d96023f57c2b8701477f0d
Sol-IoTiv <70333661+Sol-IoTiv@users.noreply.github.com>
2026-09-22 11:43:32 +0200

    Move lighting panel below detection settings and align brightness threshold in lux

--- REOCAM/module.php
+++ REOCAM/module.php
@@ -623,7 +623,6 @@ class Reolink extends IPSModuleStrict
 
         $form = [
            'elements' => [
-                $this->MotionLightingForm(),
                 [
                     'type'  => 'RowLayout',
                     'items' => [
@@ -757,6 +756,7 @@ class Reolink extends IPSModuleStrict
                         ],
                     ],
                 ],
+                $this->MotionLightingForm(),
             ],
 
                 'actions' => [
